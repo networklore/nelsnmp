b1c019259e66d503a0036fce06b49ebbb8cdf1e8
ogenstad <patrick@ogenstad.com>
2015-02-15 16:35:40 +0100

    Added snmp_get & snmp_getnext

--- nelsnmp/snmp.py
+++ nelsnmp/snmp.py
@@ -19,7 +19,54 @@ class SnmpHandler(object):
                 self.version = kwargs[key]
             if key == 'community':
                 self.community = kwargs[key]
+            if key == 'host':
+                self.host = kwargs[key]
 
         if self.version == False or self.host == False:
             print "You have to set version and host"
+        if self.version == "2c":
+            self.snmp_auth = cmdgen.CommunityData(self.community)
+
+    def snmp_get(self, *oidlist):
+
+        snmp_query = []
+        for oid in oidlist:
+            snmp_query.append(cmdgen.MibVariable(oid,), )
+
+        cmdGen = cmdgen.CommandGenerator()
+        errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+            self.snmp_auth,
+            cmdgen.UdpTransportTarget((self.host, self.port)),
+            *snmp_query
+        )
+
+        if errorIndication:
+            nelmon.common.exit_with_error(errorIndication)
+
+        if errorStatus:
+            nelmon.common.exit_with_error(errorStatus)
+
+        return varBinds
+
+
+    def snmp_getnext(self, *oidlist):
+
+        snmp_query = []
+        for oid in oidlist:
+            snmp_query.append(cmdgen.MibVariable(oid,), )
+
+        cmdGen = cmdgen.CommandGenerator()
+        errorIndication, errorStatus, errorIndex, varBinds = cmdGen.nextCmd(
+            self.snmp_auth,
+            cmdgen.UdpTransportTarget((self.host, self.port)),
+            *snmp_query
+        )
+
+        if errorIndication:
+            nelmon.common.exit_with_error(errorIndication)
+
+        if errorStatus:
+            nelmon.common.exit_with_error(errorStatus)
+
+        return varBinds
 
