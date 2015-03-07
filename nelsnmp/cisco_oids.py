34fcb5749430e5e9e053655824fa5ac9a73eefde
ogenstad <patrick@ogenstad.com>
2015-03-07 12:08:17 +0100

    Added vlan/trunk oids

--- nelsnmp/cisco_oids.py
+++ nelsnmp/cisco_oids.py
@@ -23,6 +23,22 @@ class CiscoOids(GeneralOids):
         self.vtpVlanEditDot10Said = "1.3.6.1.4.1.9.9.46.1.4.2.1.6"
         self.vtpVlanEditRowStatus = "1.3.6.1.4.1.9.9.46.1.4.2.1.11"
 
+        self.vlanTrunkPortEncapsulationType = "1.3.6.1.4.1.9.9.46.1.6.1.1.3"
+        self.vlanTrunkPortVlansEnabled = "1.3.6.1.4.1.9.9.46.1.6.1.1.4"
+        self.vlanTrunkPortNativeVlan = "1.3.6.1.4.1.9.9.46.1.6.1.1.5"
+        self.vlanTrunkPortDynamicState = "1.3.6.1.4.1.9.9.46.1.6.1.1.13"
+        self.vlanTrunkPortDynamicStatus = "1.3.6.1.4.1.9.9.46.1.6.1.1.14"
+        self.vlanTrunkPortEncapsulationOperType = "1.3.6.1.4.1.9.9.46.1.6.1.1.16"
+        self.vlanTrunkPortVlansEnabled2k = "1.3.6.1.4.1.9.9.46.1.6.1.1.17"
+        self.vlanTrunkPortVlansEnabled3k = "1.3.6.1.4.1.9.9.46.1.6.1.1.18"
+        self.vlanTrunkPortVlansEnabled4k = "1.3.6.1.4.1.9.9.46.1.6.1.1.19"
+        self.vlanTrunkPortSetSerialNo = "1.3.6.1.4.1.9.9.46.1.6.2"
+
+
+        # From CISCO-VLAN-MEMBERSHIP-MIB
+        self.vmVlan = "1.3.6.1.4.1.9.9.68.1.2.2.1.2"
+
+
         # From CISCO-CONFIG-COPY-MIB
         self.ccCopySourceFileType = "1.3.6.1.4.1.9.9.96.1.1.1.1.3"
         self.ccCopyDestFileType = "1.3.6.1.4.1.9.9.96.1.1.1.1.4"
@@ -31,4 +47,8 @@ class CiscoOids(GeneralOids):
 
         # From CISCO-FIREWALL-MIB
         self.cfwConnectionStatValue = "1.3.6.1.4.1.9.9.147.1.2.2.2.1.5"
+
+        # From CISCO-L2L3-INTERFACE-CONFIG-MIB
+        self.cL2L3IfModeAdmin = "1.3.6.1.4.1.9.9.151.1.1.1.1.1"
+
 
