82e0c83ef5968c932c1f151fe729b4c5e566f692
ogenstad <patrick@ogenstad.com>
2015-07-25 22:15:40 +0200

    Added long_description

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
-from nelsnmp import __version__
-from nelsnmp import __author__
+import re
+
+from codecs import open
 from setuptools import setup
+
+version = ''
+with open('nelsnmp/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+
+long_description = readme + '\n\n' + history
 
 config = {
     'name': 'nelsnmp',
     'packages': ['nelsnmp'],
-    'version': __version__,
+    'version': version,
     'description': 'A wrapper module for pysnmp',
-    'author': __author__,
+    'long_description': long_description,
+    'author': 'Patrick Ogenstad',
     'author_email': 'patrick@ogenstad.com',
     'license': 'Apache',
     'url': 'http://networklore.com/nelsnmp/',
@@ -15,7 +33,6 @@ config = {
     'classifiers': ['Development Status :: 4 - Beta',
                     'Intended Audience :: Developers',
                     'Intended Audience :: System Administrators']
-} 
+}
 
 setup(**config)
-
