ce60ad3376fa4a4c45e1fd66a425b50bb5de4058
ogenstad <patrick@ogenstad.com>
2015-08-17 23:03:38 +0200

    More platforms

--- lib/nelsnmp/vendors/cisco/versions.py
+++ lib/nelsnmp/vendors/cisco/versions.py
@@ -5,7 +5,17 @@ class CiscoVersion(DeviceVersion):
 
     def _get_version(self):
         for line in self._descriptions:
-            if 'Cisco IOS Software' in line:
+            if 'IOS-XE Software' in line:
+                self.os = 'ios-xe'
+                parts = line.split(',')
+                for part in parts:
+                    if 'Version' in part:
+                        try:
+                            self.version = part.split()[1]
+                        except:
+                            pass
+                break
+            elif 'Cisco IOS Software' in line:
                 self.os = 'ios'
                 parts = line.split(',')
                 for part in parts:
@@ -19,3 +29,24 @@ class CiscoVersion(DeviceVersion):
                 self.os = 'asa'
                 parts = line.split()
                 self.version = parts[-1]
+            elif 'Cisco IOS XR Software' in line:
+                self.os = 'ios-xr'
+                parts = line.split(',')
+                for part in parts:
+                    if 'Version' in part:
+                        try:
+                            self.version = part.split()[1]
+                        except:
+                            pass
+                break
+            elif 'Cisco NX-OS' in line:
+                self.os = 'nxos'
+                parts = line.split(',')
+                for part in parts:
+                    if 'Version' in part:
+                        try:
+                            self.version = part.split()[1]
+                            break
+                        except:
+                            pass
+                break
