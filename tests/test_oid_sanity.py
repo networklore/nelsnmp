0c961580df29dc32be8e56bd0c9c8a4ab3d8dd48
ogenstad <patrick@ogenstad.com>
2015-08-26 19:52:49 +0200

    Check if oid files are broken

---
+++ tests/test_oid_sanity.py
@@ -0,0 +1,18 @@
+from nelsnmp.vendors.airespace.oids import AirespaceOids
+from nelsnmp.vendors.cisco.oids import CiscoOids
+from nelsnmp.vendors.synology.oids import SynologyOids
+
+
+def test_airespace_oids_not_broken():
+    o = AirespaceOids()
+    assert o.agentInventoryProductVersion == '1.3.6.1.4.1.14179.1.1.1.14'
+
+
+def test_cisco_oids_not_broken():
+    o = CiscoOids()
+    assert o.cdpGlobalRun == '1.3.6.1.4.1.9.9.23.1.3.1'
+
+
+def test_synology_oids_not_broken():
+    o = SynologyOids()
+    assert o.synoSystem == '1.3.6.1.4.1.6574.1'
