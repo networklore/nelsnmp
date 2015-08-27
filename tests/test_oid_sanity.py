4455299532ab2a3bde1040dc8ba73009c9ad2c7e
ogenstad <patrick@ogenstad.com>
2015-08-27 18:09:41 +0200

    Old Cisco Oids

--- tests/test_oid_sanity.py
+++ tests/test_oid_sanity.py
@@ -1,7 +1,7 @@
 from nelsnmp.vendors.airespace.oids import AirespaceOids
 from nelsnmp.vendors.cisco.oids import CiscoOids
 from nelsnmp.vendors.synology.oids import SynologyOids
-
+from nelsnmp.cisco_oids import CiscoOids as OldCiscoOids
 
 def test_airespace_oids_not_broken():
     o = AirespaceOids()
@@ -10,6 +10,11 @@ def test_airespace_oids_not_broken():
 
 def test_cisco_oids_not_broken():
     o = CiscoOids()
+    assert o.cdpGlobalRun == '1.3.6.1.4.1.9.9.23.1.3.1'
+
+
+def test_old_cisco_oids_not_broken():
+    o = OldCiscoOids()
     assert o.cdpGlobalRun == '1.3.6.1.4.1.9.9.23.1.3.1'
 
 
