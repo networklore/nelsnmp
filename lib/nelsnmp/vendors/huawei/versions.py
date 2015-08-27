9d0ba0ebed896f048699f6f69896518acf64cce2
ogenstad <patrick@ogenstad.com>
2015-08-27 18:10:19 +0200

    Get Huawei VRP version

--- lib/nelsnmp/vendors/huawei/versions.py
+++ lib/nelsnmp/vendors/huawei/versions.py
@@ -7,3 +7,12 @@ class HuaweiVersion(DeviceVersion):
         for line in self._descriptions:
             if 'VRP' in line:
                 self.os = 'vrp'
+                parts = line.split()
+                if len(parts) > 5:
+                    if parts[2][-7:] == 'Version':
+                        self.version = parts[3]
+                    if parts[5].endswith(')'):
+                        if self.version:
+                            self.version += ' - ' + parts[5][:-1]
+                        else:
+                            self.version = parts[5][:-1]
